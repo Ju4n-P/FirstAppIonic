99f61adc266398037feab64d695cf96f3b8213d6
J <juancarlospecan@gmail.com>
2021-03-22 11:26:13 +0100

    premier compo ionc header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './component/page1/page1.component';
+import { Page2Component } from './component/page2/page2.component';
+import { Page3Component } from './component/page3/page3.component';
+import { Page4Component } from './component/page4/page4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'page1', component: Page1Component},
+  {path: 'page2', component: Page2Component},
+  {path: 'page3', component: Page3Component},
+  {path: 'page4', component: Page4Component},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
